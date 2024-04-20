a49f1ced69100bcefd8fda5c1d49bc7967180fa9
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2024-04-20 22:04:55 +0200

    dodawanie nowej transakcji do modelu przez kontroler

--- skarbonka.js
+++ skarbonka.js
@@ -2,30 +2,26 @@ const model = {
     filtrujTyp: "",
     filtrujKategoria: "",
 
-    transactions: [
+    transakcje: [
         {
-            id: 1,
             data: "2024-04-16",
             wartość: 20,
             typ: "wydatek",
             kategoria: "jedzenie"
         },
         {
-            id: 2,
             data: "2024-04-16",
             wartość: 200,
             typ: "wydatek",
             kategoria: "lekarz"
         },
         {
-            id: 3,
             data: "2024-04-16",
             wartość: 1000,
             typ: "przychód",
             kategoria: "Pensja"
         },
         {
-            id: 4,
             data: "2024-04-16",
             wartość: 100000,
             typ: "przychód",
@@ -47,14 +43,7 @@ const view = {
         this.dodajBTN = document.getElementById("dodajBTN");
 
         // po naciśnieciu guzika zapisanie wpisanych wartości do obiektu
-        this.nowaTransakcja = {};
-
-        this.dodajBTN.addEventListener("click", () => {
-            this.nowaTransakcja.data = this.data.value;
-            this.nowaTransakcja.wartosc = this.wartosc.value;
-            this.nowaTransakcja.typ = this.dtyp.value;
-            this.nowaTransakcja.kategoria = this.dkategoria.value;
-        });
+        this.dodajBTN.addEventListener("click", () => controller.addTransakcja());
 
 
         // FILTROWANIE HISTORII TRANSAKCJI
@@ -78,7 +67,20 @@ const controller = {
     setFiltry(){
         model.filtrujTyp = view.ftyp.value;
         model.filtrujKategoria = view.fkategoria.value;
+    },
+
+    addTransakcja(){
+        nowaTransakcja = {};
+
+        nowaTransakcja.data = view.data.value;
+        nowaTransakcja.wartosc = view.wartosc.value;
+        nowaTransakcja.typ = view.dtyp.value;
+        nowaTransakcja.kategoria = view.dkategoria.value;
+
+        model.transakcje.push(nowaTransakcja);
     }
+
+
 
 }
 
