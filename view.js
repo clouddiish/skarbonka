d0222ac253db00aa4092e31d8b1ed34bc01082b0
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2024-04-20 20:23:35 +0200

    dodawanie nowej traksakcji i jeden guzik do filtrowania

--- view.js
+++ view.js
@@ -1,23 +1,41 @@
 const view = {
 
 	init() {
+
+		// DODAWANIE TRANSAKCJI
+
+		this.data = document.getElementById("data");
+		this.wartosc = document.getElementById("wartosc");
+		this.dtyp = document.getElementById("dtyp");
+		this.dkategoria = document.getElementById("dkat");
+		this.dodajBTN = document.getElementById("dodajBTN");
+
+		// po naciśnieciu guzika zapisanie wpisanych wartości do obiektu
+		this.nowaTransakcja = {};
+
+		this.dodajBTN.addEventListener("click", () => {
+			this.nowaTransakcja.data = this.data.value;
+			this.nowaTransakcja.wartosc = this.wartosc.value;
+			this.nowaTransakcja.typ = this.dtyp.value;
+			this.nowaTransakcja.kategoria = this.dkategoria.value;
+		});
+
+
+		// FILTROWANIE HISTORII TRANSAKCJI
+
 		this.ftyp = document.getElementById("ftyp");
-		this.ftypBTN = document.getElementById("ftypBTN");
 		this.ftypValue = "";
 
 		this.fkategoria = document.getElementById("fkat");
-		this.fkategoriaBTN = document.getElementById("fkatBTN");
 		this.fkategoriaValue = "";
 
+		this.filtrujBTN = document.getElementById("filtrujBTN");
+
 		// po naciśnięciu guzików aktualizacja wartości
-		this.ftypBTN.addEventListener("click", () => {
+		this.filtrujBTN.addEventListener("click", () => {
             this.ftypValue = this.ftyp.value;
+			this.fkategoriaValue = this.fkategoria.value;
 		});
-
-		this.fkategoriaBTN.addEventListener("click", () => {
-            this.fkategoriaValue = this.fkategoria.value;
-		});
-
 
 	},
 
